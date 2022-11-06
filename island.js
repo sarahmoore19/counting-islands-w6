4a004d17762a6469525fbf9a65ff099cb1afff57
sarahmoore <sarahnmoore19@gmail.com>
2022-11-06 12:04:03 -0500

    found number of islands in graps

--- island.js
+++ island.js
@@ -1,38 +1,104 @@
-function getNeighbors(row, col, matrix) {
-  
-  // Check top
-  // Check top right
-  // Check right
-  // Check bottom right
-  // Check bottom
-  // Check bottom left
-  // Check left
-  // Check top left
-  // Return neighbors
-  
-  // Your code here
+// returns an array of all adjacent nodes that are 1's inclding diagonals
+function getNeighbors(row, column, matrix) {
+  let newArr = [];
+
+    //left top diagonal = row - 1, column - 1;
+    if (row - 1 >= 0 && column - 1 >= 0 ) {
+      if (matrix[row - 1][column - 1] === 1) {
+        newArr.push([row - 1, column - 1]);
+      }
+    }
+
+    //top = row - 1;
+    if (row - 1 >= 0 && matrix[row - 1].length >= column + 1) {
+      if (matrix[row - 1][column] === 1) {
+        newArr.push([row - 1, column])
+      }
+    }
+
+    //right top diagonal = row - 1, column + 1;
+    if (row - 1 >= 0 && matrix[row - 1].length >= column + 2) {
+      if (matrix[row - 1][column + 1] === 1) {
+        newArr.push([row - 1, column + 1]);
+      }
+    }
+
+    //left = column - 1;
+    if (column - 1 >= 0) {
+      if (matrix[row][column - 1] === 1) {
+        newArr.push([row, column - 1]);
+      }
+    }
+
+    //right = column + 1;
+    if (matrix[row].length >= column + 2) {
+      if (matrix[row][column + 1] === 1) {
+        newArr.push([row, column + 1])
+      }
+    }
+
+    //left bottom diagonal = row + 1, column - 1;
+    if (matrix.length >= row + 2 && column - 1 >= 0) {
+      if (matrix[row + 1][column - 1] === 1) {
+        newArr.push([row + 1, column - 1]);
+      }
+    }
+
+    //bottom = row + 1
+    if (matrix.length >= row + 2 && matrix[row + 1].length >= column + 1) {
+      if (matrix[row + 1][column] === 1) {
+        newArr.push([row + 1, column]);
+      }
+    }
+
+    //right bottom diagonal = row + 1; bottom + 1;
+    if (matrix.length >= row + 2 && matrix[row + 1].length >= column + 2) {
+      if (matrix[row + 1][column + 1] === 1) {
+        newArr.push([row + 1, column + 1]);
+      }
+    }
+
+    return newArr;
+  }
+
+// returns array of all nodes that could start an island(1);
+function findStarts(matrix) {
+  let newArr = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] === 1) newArr.push([i, j])
+    }
+  }
+  return newArr;
+}
+
+function traversal(node, set, matrix) {
+  let stack = [node];
+  set.add(node.join(","))
+  while (stack.length) {
+    let curr = stack.pop();
+    let neighbors = getNeighbors(curr[0], curr[1], matrix);
+    for (let node of neighbors) {
+      if (!set.has(node.join(","))) {
+        stack.push(node);
+        set.add(node.join(","));
+      }
+    }
+  }
+  return set;
 }
 
 function countIslands(matrix) {
-  
-  // Create a visited set to store visited nodes
-  // Initialize count to 0
-  // Iterate through all indices in matrix
-    // If an index contains a 1 and has not been visited, 
-    // increment island count and start traversing neighbors
-      // DO THE THING (increment island count by 1)
-      // Initialize a stack with current index
-      // Add stringified version of current index to the visited set
-      // While stack contains elements
-        // Pop element from stack
-        // Get valid neighbors of current element
-        // Iterate over neigbors
-          // If neighbor has not been visited
-            // Add neighbor to stack
-            // Mark neighbor as visited
-  // Return island count
-  
-  // Your code here
+  let starts = findStarts(matrix);
+  let set = new Set();
+  let count = 0;
+  for (let node of starts) {
+    if (!set.has(node.join(","))) {
+      count++;
+      set = traversal(node, set, matrix);
+    }
+  }
+  return count;
 }
 
 // Uncomment the lines below for local testing
